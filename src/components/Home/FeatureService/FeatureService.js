21c8058c7c7c8601a7f1c9db5db0e2da068a81e0
Nayemul <nayemulhasan2016@gmail.com>
2021-04-11 02:18:03 +0600

    feature services done

---
+++ src/components/Home/FeatureService/FeatureService.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const FeatureService = () => {
+    return (
+        <section className="feature-section pb-0 pb-md-5 my-5">
+            <div className="container mb-5">
+                <div className="row mb-5">
+                    <div className="col-md-5 mb-4 m-md-0">
+                        <img className="img-fluid" src="https://i.postimg.cc/sfhsrXN7/Mask-Group-3.png" alt=""/>
+                    </div>
+                    <div className="col-md-7 align-self-center">
+                        <h1>Exceptional Dental Care, on Your Term</h1>
+                        <p className="text-secondary my-5">Good dental or oral care is important to maintaining healthy teeth, gums and tongue. Oral problems, including bad breath, dry mouth, canker or cold sores, TMD, tooth decay, or thrush are all treatable with proper diagnosis and care.</p>
+                        <button className="btn btn-primary appoimentBtn">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default FeatureService;
