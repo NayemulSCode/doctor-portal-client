cb7d5312bfe375165f4082490004e18f97172c09
Nayemul <nayemulhasan2016@gmail.com>
2021-04-12 23:18:11 +0600

    done

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -5,32 +5,32 @@ import { UserContext } from '../../../App';
 const Navbar = () => {
     const [loggedInUser, setLoggedInUser] =useContext(UserContext);
     return (
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <a href="">{loggedInUser.email}</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <Link to="/">{loggedInUser.email}</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <Link class="nav-link ms-5 active" aria-current="page" to="/">Home</Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link ms-5 active" aria-current="page" to="/">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link ms-5 " to="/appointment">Appointment</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link ms-5 " to="/appointment">Appointment</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link ms-5" to="/login">Login</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link ms-5" to="/login">Login</Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link ms-5 text-white" href="#">Dental Service</a>
+                        <li className="nav-item">
+                            <a className="nav-link ms-5 text-white" href="#">Dental Service</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link ms-5 text-white" href="#">Reviews</a>
+                        <li className="nav-item">
+                            <a className="nav-link ms-5 text-white" href="#">Reviews</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link ms-5 text-white" href="#">Contact Us</a>
+                        <li className="nav-item">
+                            <a className="nav-link ms-5 text-white" href="#">Contact Us</a>
                         </li>
                     </ul>
                 </div>
